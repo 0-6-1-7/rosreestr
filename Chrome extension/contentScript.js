bfd2633b81990b43ed14ba0941ade0c1cd1900d6
0-6-1-7 <60964734+0-6-1-7@users.noreply.github.com>
2020-02-14 16:45:05 +0400

    Minor update
    Распознавание капчи работает на всех страницах раздела 
    Сервисы  > Справочная информация по объектам недвижимости в режиме online

--- Chrome extension/contentScript.js
+++ Chrome extension/contentScript.js
@@ -4,7 +4,7 @@ function httpGetAsync(imgData, eleId) {
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.open("GET", "http://localhost:8111/cgi-bin/captcha.py?imgData='" + imgData + "'", true);
 	xmlHttp.onload = function() { eleId.value = this.responseText; };
-	xmlHttp.onerror = function() { eleId.value = '-----'; };
+	xmlHttp.onerror = function() { eleId.value = "-----"; };
     xmlHttp.send(null);
 }
 
@@ -20,16 +20,30 @@ function getBase64Image(img) {
 }
 
 function base64_url_encode(url) {
- return url.replace(/\+/g, '.').replace(/\//g, '_').replace(/\=/g, '-');
+ return url.replace(/\+/g, ".").replace(/\//g, "_").replace(/\=/g, "-");
 }
 
 function getCaptcha() {
-	var captcha = document.getElementById("captchaImage2");
-	if(!captcha.complete) { console.log('image not complete'); return; }
+	if (document.title.length < 1) { return; }
+	if (document.URL.indexOf("https://rosreestr.ru/wps/portal/online_request") >= 0 ||
+		document.URL.indexOf("https://rosreestr.ru/wps/portal/p/cc_ib_portal_services/online_request/") >= 0) {
+		var captcha = document.getElementById("captchaImage2");
+		var captchaField = document.getElementsByName("captchaText")[0];
+	}
+	else if (document.URL.indexOf("https://rosreestr.ru/wps/portal/p/cc_present/ir_egrn") >= 0){
+		var ibmMainContainer = document.getElementsByName("ibmMainContainer")[0];
+		if (ibmMainContainer.getElementById("gwt-uid-3") == null) { return; }
+		var captcha = ibmMainContainer.querySelector("img");
+		var captchaField = ibmMainContainer.querySelector("input");
+		ibmMainContainer.getElementById("gwt-uid-3").click();
+		ibmMainContainer.querySelector("div.v-button").focus();
+	} 
+	else {return;}
+	if(!captcha.complete) { return; }
 	if (oldCaptchaSrc != captcha.src) {
 		oldCaptchaSrc = captcha.src;
-		solvedCaptcha = httpGetAsync(base64_url_encode(getBase64Image(captcha)), document.getElementsByName("captchaText")[0]);
+		solvedCaptcha = httpGetAsync(base64_url_encode(getBase64Image(captcha)), captchaField);
 	}
 }
 
-let timerId = setInterval(() => getCaptcha(), 100);
+let timerId = setInterval(() => getCaptcha(), 500);
