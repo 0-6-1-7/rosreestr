2429e411b934d92c454d80591228a70bfc1b6f45
0-6-1-7 <60964734+0-6-1-7@users.noreply.github.com>
2020-12-05 23:47:22 +0400

    Add files via upload

--- Chrome extension v.2.2/gatherData.js
+++ Chrome extension v.2.2/gatherData.js
@@ -7,25 +7,33 @@ if (document.URL.match('\.*object_data_id=\.*') != null) {
 	document.querySelector("div.portlet-side-menu").style.visibility = "hidden";
 	document.querySelector("div.content-right").style.marginTop = "-90px";
 
-var cc = 0;
-var check1 = setInterval(function() {
-	var elem1 = document.getElementById("sw_r_enc");
-	if (elem1 != null || cc > 99) {
-		clearInterval(check1);
-		if (r_enc.style.display == "none") { sw_r_enc.click(); }
-	}
-	cc = cc + 1
-}, 100); // check every 100ms
+	var cc = 0;
+	var elem1;
+	var elem2;
 
-var check2 = setInterval(function() {
-	var elem2 = document.getElementById("sw_s_notes");
-	if (elem2 != null || cc > 100) {
-		clearInterval(check2);
-		if (s_notes.style.display == "none") { sw_s_notes.click(); }
-	}
-	cc = cc + 1
-}, 100); // check every 100ms
+	var check1 = setInterval(function() {
+		elem1 = document.getElementById("sw_r_enc");
+		if (elem1 != null || cc > 100) {
+			clearInterval(check1);
+			console.log("sw_r_enc exists",cc);
+		} else {
+			console.log("sw_r_enc doesn't exist",cc);
+			cc = cc + 1
+		}
+	}, 100); // check every 100ms
+	setTimeout(() => { if (r_enc.style.display == "none") { sw_r_enc.click(); } }, 500);
+	
 
-	
-	
+	var check2 = setInterval(function() {
+		elem2 = document.getElementById("sw_s_notes");
+		if (elem2 != null || cc > 100) {
+			clearInterval(check2);
+			console.log("sw_s_notes exists",cc);
+		} else {
+			console.log("sw_s_notes doesn't exist",cc);
+			cc = cc + 1
+		}
+	}, 100); // check every 100ms
+	setTimeout(() => { if (s_notes.style.display == "none") { sw_s_notes.click(); } }, 500);
+
 }
