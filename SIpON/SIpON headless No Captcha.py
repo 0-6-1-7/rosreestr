536009356f13ddae89ec5e6d207e6542fdcc58f6
0-6-1-7 <60964734+0-6-1-7@users.noreply.github.com>
2021-04-04 21:41:34 +0300

    Add files via upload

--- SIpON/SIpON headless No Captcha.py
+++ SIpON/SIpON headless No Captcha.py
@@ -4,10 +4,10 @@ from recognize import recognize
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-import openpyxl, os, time
+import openpyxl, os, sys, time
 
 SIpON = None
-status = 0 # состояние сайта
+localStatus = 0 # состояние сайта
   
 ##------------------------------------------------------------##
 def SIpONinit():
@@ -15,13 +15,29 @@ def SIpONinit():
   chrome_options = Options()  
   chrome_options.add_argument("--headless")
   SIpON = webdriver.Chrome(options = chrome_options)
-  SIpON.implicitly_wait(2)
-  SIpON.set_page_load_timeout(30)
-  try:
-    SIpON.get("https://rosreestr.ru/wps/portal/online_request")
-  except:
-    print("Сайт Росреестра не работает")
-    return 999
+  SIpON.implicitly_wait(5)
+  plt = 10
+  localStatus = 0
+  form = None
+##  SIpON.set_page_load_timeout(plt)
+  while True:
+    SIpON.set_page_load_timeout(plt)
+    try:
+      SIpON.get("https://rosreestr.ru/wps/portal/online_request")
+    except:
+      SIpON.execute_script("window.stop();")
+      localStatus = 999
+
+    try:
+      form = SIpON.find_element_by_id("online_request_search_form_span")
+    except:
+      plt = plt + 10
+
+    if form != None:
+      return 0
+
+    if plt > 120:
+      return 999
 
 ##------------------------------------------------------------##
 def SIpONrestart(t):
@@ -35,7 +51,6 @@ def SIpONrestart(t):
 ##------------------------------------------------------------##
 def RecognizeCaptcha(c):
   global SIpON
-  #code from https://gist.github.com/spirkaa/4c3b8ad8fd34324bd307#gistcomment-3157744
   img_base64 = SIpON.execute_script("""var ele = arguments[0];var cnv = document.createElement('canvas');cnv.width = 180; cnv.height = 50;cnv.getContext('2d').drawImage(ele, 0, 0);return cnv.toDataURL('image/png').substring(22);""", c)
   captcha = recognize(img_base64)
   return captcha
@@ -44,93 +59,106 @@ def RecognizeCaptcha(c):
 def GetInfo(KN):
   global SIpON
   i = 1
-  status = 1
-  while status != 0 and i < 10:
-    status = 0
+  localStatus = 1
+  while localStatus != 0 and i < 10:
+    localStatus = 0
     if i > 1:
       SIpONrestart(i)
-    if status == 0:
+    if localStatus == 0:
       try:
         SetSearchCritVisible = SIpON.find_element_by_css_selector("a[href*='SetSearchCritVisible']")
         SetSearchCritVisible.click()
       except:
         pass
-##      try:
-##        captchaImage2 = SIpON.find_element_by_id("captchaImage2")
-##      except:
-##        status = 999 # капчи нет, вероятно, сайт не работает
-##    if status == 0:    
-##      try:
-##        captcha = RecognizeCaptcha(captchaImage2)
-##      except:
-##        status = 999 # капча не проявилась
-##        pass
-    if status == 0:
+    if localStatus == 0:
       try:
         SIpON.execute_script("arguments[0].click();", SIpON.find_element_by_id("cad-number"))
         cadnum = SIpON.find_element_by_css_selector("input[type='text'][name='cad_num']")
         SIpON.execute_script("arguments[0].click();", cadnum)
         cadnum.clear()
         cadnum.send_keys(KN)
-##        captchaText = SIpON.find_element_by_css_selector("input[type='text'][name='captchaText']")
-##        SIpON.execute_script("arguments[0].click();", captchaText)
-##        captchaText.send_keys(captcha)
         SIpON.execute_script("arguments[0].click();", SIpON.find_element_by_id("submit-button"))
       except:
-        status = 999
-    if status == 0:
+        SIpON.execute_script("window.stop();")
+
+    rc = 0
+    while True:
       try:
         link = SIpON.find_element_by_css_selector("a[href*='object_data_id']").get_attribute("href")
+        break
       except:
-        status = 998
-    if status == 998:
+        time.sleep(2)
+        rc = rc + 1
+      if rc > 4:
+        localStatus = "No Result"
+        break
+
+    if localStatus == "No Result":
       try:
         infomsg = SIpON.find_element_by_css_selector("td.infomsg1")
         if infomsg != None:
           return f"{KN}\tОбъект не найден"
       except:
-        status = 999
-    if status == 0:
+        localStatus = "Error"
+        
+    if localStatus == 0:
       mainwindow = SIpON.current_window_handle
       SIpON.execute_script("window.open('" + link + "');")
-      SIpON.switch_to.window(SIpON.window_handles[1])  
-      try:
-        r_enc = SIpON.find_element_by_id("r_enc")
-        if "none" in r_enc.get_attribute("style"):
+      SIpON.switch_to.window(SIpON.window_handles[1])
+      
+      rc = 0
+      sw_r_enc = None
+      r_enc = None
+      while True:
+        try:
           sw_r_enc = SIpON.find_element_by_id("sw_r_enc")
+          r_enc = SIpON.find_element_by_id("r_enc")
+        except:
+          pass
+        if sw_r_enc == None or r_enc == None:
+          time.sleep(2)
+          rc = rc + 1
+          print(f"Попытка {rc}")
+        else:
+          break
+        if rc > 4:
+          break
+        
+      try:
+        if "none" in r_enc.get_attribute("style"):
           SIpON.execute_script("arguments[0].scrollIntoView();", sw_r_enc)
           sw_r_enc.click()
-      except:
-        pass
-      try:
-        s_notes = SIpON.find_element_by_id("s_notes")
-        if "none" in s_notes.get_attribute("style"):
-          sw_s_notes = SIpON.find_element_by_id("sw_s_notes")
-          sw_s_notes.click()
       except:
         pass
       try:
         content = SIpON.find_element_by_css_selector("div.portlet-body").get_attribute("innerText")
         t = Convert(content)
       except:
-        status = 999
+        localStatus = 999
       finally:
         SIpON.close()
         SIpON.switch_to.window(mainwindow)  
-    if status == 999:
+    if localStatus == 999:
       i = i + 1
   else:
-    if status == 999:
+    if localStatus == 999:
       return f"{KN}\tНе удалось выполнить за 10 попыток"
-    elif status == 0:
+    elif localStatus == 0:
       return t # OK
 
 ##------------------------------------------------------------##
 SIpONinit()
-p = os.path.split(os.path.dirname(os.path.abspath(__file__)))[1]
+
+wbName = 'KN.xlsx'
+if len(sys.argv) > 1:
+  wbName = sys.argv[1]
+else:
+  wbName = 'KN.xlsx'
+
+p = os.path.split(os.path.dirname(os.path.abspath(__file__)))[1] + "\\" + wbName
 
 from openpyxl import load_workbook
-wb = load_workbook(filename = 'KN.xlsx')
+wb = load_workbook(filename = wbName)
 ws1 = wb.worksheets[0]
 ws2 = wb.create_sheet("Sheet KN")
 
@@ -146,19 +174,18 @@ ls = ws1.cell(row = r , column = 2).value
 while cc != None:
   t = GetInfo(cc)
 
-  print(f"----- [ {r - 1} из {rmax} ] ----- [ {p} ] ----------------------------------------------")
+  print(f"-- [ {r - 1} из {rmax} ] -- [ {p} ] ----------------------------------------------")
   print(t)
   tl = t.split("\t", 10)
   ws2.cell(row = r, column = 1).value = ls
   for l in range(0, len(tl)):
     ws2.cell(row = r, column = l + 2).value = tl[l]
-##    print(f"{l}: {tl[l - 1]}")
   if (r - 1) % 10 == 0:
-    wb.save("KN.xlsx")
+    wb.save(wbName)
   r = r + 1
   cc = ws1.cell(row = r, column = 1).value
   ls = ws1.cell(row = r, column = 2).value
 
-wb.save("KN.xlsx")
+wb.save(wbName)
 print(f"\n\n\n\n\nВсё готово! ----- [ {p} ] ----- [ {time.strftime('%H:%M:%S', time.localtime())} ] ")
 print()
