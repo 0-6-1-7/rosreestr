e3266bda30c2cf68127bbbe269a278a8e1ecaca0
Aleksei Mihalin <warmwavemedia@gmail.com>
2021-07-15 23:13:22 +0300

    add global config

---
+++ config.py
@@ -0,0 +1,5 @@
+isHeadless = False
+
+CaptchaReloadingCounter = 0  # Счётчик попыток перезагрузить капчу
+CaptchaReloadingCounterMax = 30  # Максимум попыток
+SaveCaptcha = False  # сохранение капчи в файл
