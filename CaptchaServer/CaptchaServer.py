e9d18f9f54d4fa9164c4f983e175bb1d82cbd12e
0-6-1-7 <60964734+0-6-1-7@users.noreply.github.com>
2021-11-07 03:36:26 +0300

    Delete CaptchaServer directory

--- CaptchaServer/CaptchaServer.py
+++
@@ -1,5 +0,0 @@
-from http.server import HTTPServer, CGIHTTPRequestHandler
-
-server_address = ("", 8111)
-CaptchaServer = HTTPServer(server_address, CGIHTTPRequestHandler)
-CaptchaServer.serve_forever()
--- CaptchaServer/CaptchaServerMultiThreaded.py
+++
@@ -1,9 +0,0 @@
-from http.server import HTTPServer, CGIHTTPRequestHandler
-from socketserver import ThreadingMixIn
-import threading
-
-class ThreadingSimpleServer(ThreadingMixIn, HTTPServer):
-    pass
-
-CaptchaServerMultiThreaded = ThreadingSimpleServer(("", 8111), CGIHTTPRequestHandler)
-CaptchaServerMultiThreaded.serve_forever()
