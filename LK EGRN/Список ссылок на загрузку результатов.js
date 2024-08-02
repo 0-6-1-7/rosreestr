9013a4fca67c41100b71bd8075d9d681fbe9ab48
0-6-1-7 <60964734+0-6-1-7@users.noreply.github.com>
2024-08-02 21:51:54 +0300

    Список ссылок на загрузку результатов.js

---
+++ LK EGRN/Список ссылок на загрузку результатов.js
@@ -0,0 +1,42 @@
+function LinksList() {
+  try {
+    const query = 'from:noreply-site@rosreestr.ru is:unread subject:Заявление с номером OfSite-* исполнено';
+    const threads = GmailApp.search(query);
+
+    if (threads.length === 0) {
+      console.log('Нет подходящих сообщений');
+    } else {
+      let result = '';
+      let myEmail = '';
+      for (thread of threads) {
+        const messages = thread.getMessages();
+        for (message of messages) {
+          const messageBody = message.getBody();
+          const messageSubj = message.getSubject()
+          const link = getLinkFromBody(messageBody);
+          myEmail = message.getTo()
+          result += String(`<a href="${link}">${messageSubj}<br>`);
+          GmailApp.starMessage(message);
+          GmailApp.markMessageRead(message);
+        }
+      }
+      GmailApp.sendEmail(myEmail, 'Ссылки на загрузку результатов', '', {
+        htmlBody: result
+        })
+    }
+  } catch (err) {
+    console.log(`При выполнении скрипта возникла ошибка: ${err.toString()}`);
+  }
+}
+
+function getLinkFromBody(body) {
+  const linkRegex = /<a\s+(?:[^>]*?\s+)?href="([^"]*)"/g;
+  let links;
+  let match;
+
+  if ((match = linkRegex.exec(body)) !== null) {
+    links = match[1];
+  }
+
+  return links;
+}
