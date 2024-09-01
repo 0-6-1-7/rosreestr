0c000559d9127bbab40bde3607640e3078e0fdbd
0-6-1-7 <60964734+0-6-1-7@users.noreply.github.com>
2024-09-01 04:10:11 +0300

    Add files via upload

---
+++ Chrome extension v.3.1/scripts/background.js
@@ -0,0 +1,49 @@
+chrome.runtime.onInstalled.addListener(() => {
+	chrome.action.setBadgeText({ text: 'Выкл', });
+	chrome.storage.local.set({ state: 'Выкл' }).then(() => {});
+});
+
+const rr = 'https://lk.rosreestr.ru/eservices/real-estate-objects-online';
+
+chrome.action.onClicked.addListener(async (tab) => {
+	if (tab.url.startsWith(rr)) {
+		const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
+		let nextState = '';
+		switch (prevState) {
+			case 'Выкл': 
+				nextState = '34';
+				break;
+			case '34':
+				nextState = 'i';
+				break;
+			case 'i':
+				nextState = 'Выкл';
+				break;
+		};
+
+		await chrome.action.setBadgeText({
+			tabId: tab.id,
+			text: nextState,
+		});
+		
+		chrome.storage.local.set({ state: nextState }).then(() => {});
+		
+		if (nextState === '34') { //inject CSS just once
+			await chrome.scripting.insertCSS({
+				files: ['styles/focus-mode.css'],
+				target: { tabId: tab.id },
+			});
+		} else if (nextState === 'Выкл') {
+			try {
+				await chrome.scripting.removeCSS({
+					files: ['styles/focus-mode.css'],
+					target: { tabId: tab.id },
+				});
+			} catch (err) {
+				console.error(`failed to remove CSS: ${err}`);
+			};
+		};
+	};
+});
+
+
---
+++ Chrome extension v.3.1/scripts/content.js
@@ -0,0 +1,114 @@
+async function writeClipboardText(element1, element2) {
+//element1 - кадастровый номер (для раскрашивания), element2 - элемент, из которго будет взят innerText для буфера
+	try {
+		const textToCopy = element2.innerText;
+		await navigator.clipboard.writeText(textToCopy);
+		element1.setAttribute('kn-status', 'copied');
+	} catch (error) {
+		//console.error(`Exception. ${error.name}: ${error.message}`);
+		element1.setAttribute('kn-status', 'error');
+	}
+}
+
+function clearInfo(i) {
+	return String(i).replace('\nДЕЙСТВИЯ\n', '\n');
+}
+
+function waitModal () {
+	chrome.storage.local.get(["state"]).then((result) => { 
+		const state = result.state;
+		if (state === 'Выкл') { return };
+		const m = document.querySelector('button.rros-ui-lib-modal__close-btn');
+		if (m && m.checkVisibility()) {
+			try {
+				let poe = false; //наличие раздела "Сведения о правах и ограничениях (обременениях)"
+				let soe = null; //строка "Статус объекта"
+				let soeText = ''; //значение в строке "Статус объекта" 
+				let kne = null; //строка "Кадастровый номер" в разделе "Общая информация"
+				let kneColor = ''; // цвет кадастрового номера
+				let etc = null; //элемент, из которго будет взят innerText для буфера (кадстровый номер или вся карточка)
+				elements = document.querySelectorAll('li.build-card-wrapper__info__ul__subinfo');
+				for (const e of elements) {
+					const h = e.querySelector('span.build-card-wrapper__info__ul__subinfo__name');
+					switch (h.innerText) {
+						case 'Кадастровый номер':
+							if (!kne) {
+								kne = e;
+							};
+							break;
+						case 'Статус объекта':
+							soe = e;
+							soeText = e.querySelector('div.build-card-wrapper__info__ul__subinfo__options__item__line').innerText;
+							break;
+						case 'Вид, номер и дата государственной регистрации права':
+							poe = true;
+							break;
+						default:
+					};
+				};
+				switch (soeText) {
+					case 'Актуально':
+						if (poe) {
+							soe.setAttribute('so-type', 'active')
+							kneColor = kne.style.backgroundColor;
+							knStatus = kne.getAttribute('kn-status');
+							if (knStatus != 'copied') {
+								if (window.isSecureContext) {
+									switch (state) {
+										case '34':
+											etc = kne;
+											break;
+										case 'i':
+											etc = document.querySelector('div.build-card-wrapper.card')
+											break;
+									};
+									try {
+										writeClipboardText(kne, etc);
+									} catch (error) {
+										console.error(`Exception. ${error.name}: ${error.message}`);
+									};
+								};
+							};
+						} else {
+							soe.setAttribute('so-type', 'inactive')
+						};
+						break;
+					case 'Погашено':
+						soe.setAttribute('so-type', 'closed')
+
+						break;
+				}
+			} catch (error) {
+				console.error(`Exception. ${error.name}: ${error.message}`);
+			};
+		};
+		
+	});
+	setTimeout(waitModal, 300);
+}
+
+
+function waitErrors () {
+	chrome.storage.local.get(["state"]).then((result) => {
+		const state = result.state; 
+		if (state === 'Выкл') { return };
+		const errors = document.querySelectorAll('div.rros-ui-lib-error.rros-ui-lib-error-ERROR');
+		if (errors.length > 0) {
+			for (const e of errors) {
+				console.log(`Сообщение сайта: ${e.innerText}`);
+				try {
+					const b = e.querySelector('button.rros-ui-lib-button.rros-ui-lib-button--link');
+					b.click()
+				} catch (error) {
+					console.error(`Exception. ${error.name}: ${error.message}`);
+				};
+			};
+		};
+	});
+	setTimeout(waitErrors, 5000);
+}
+
+
+//main
+waitModal();
+waitErrors();
